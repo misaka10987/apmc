cddbb3952e038bc53f0215f65e8a7a4f4f0caf22
misaka10987 <misaka10987@outlook.com>
2023-12-17 17:59:35 +0800

    Temporarily disabled autosave.

--- launch.sh
+++ launch.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 set -e
 
-sync() {
-    echo "Connecting to Cloudflare WARP:"
-    warp-cli connect
-    sleep 2
-    local branch="auto-$(date -u +%Y%m%d.%H%M%S)"
-    git checkout -b $branch
-    git add *
-    git commit -m "Autosaved."
-    git push origin $branch
-    warp-cli disconnect
-}
+# sync() {
+#     echo "Connecting to Cloudflare WARP:"
+#     warp-cli connect
+#     sleep 2
+#     local branch="auto-$(date -u +%Y%m%d.%H%M%S)"
+#     git checkout -b $branch
+#     git add *
+#     git commit -m "Autosaved."
+#     git push origin $branch
+#     warp-cli disconnect
+# }
 
 main() {
     warp-cli disconnect
     sleep 2
     java -jar "./server.jar"
     sleep 2
-    echo "Server closed. Automatically backup..."
+    # echo "Server closed. Automatically backup..."
     echo "Done."
-    sync
+    # sync
 }
 
 main
