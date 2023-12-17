689ef00845bc2e3dba47f45a6cbccf118077af35
misaka10987 <misaka10987@outlook.com>
2023-12-17 17:14:58 +0800

    Modified script.

--- launch.sh
+++ launch.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 set -e
-warp-cli disconnect
-sleep 2
-java -jar "./server.jar"
+
 sync() {
-    branch="autosave-$(date -u +%Y%m%d-%H%M%S)"
+    echo "Connecting to Cloudflare WARP:"
+    warp-cli connect
+    sleep 2
+    local branch="auto-$(date -u +%Y%m%d.%H%M%S)"
     git checkout -b $branch
     git add *
     git commit -m "Autosaved."
     git push origin $branch
-    rm ./logs/*.gz
+    warp-cli disconnect
 }
+
+main() {
+    warp-cli disconnect
+    sleep 2
+    java -jar "./server.jar"
+    sleep 2
+    echo "Server closed. Automatically backup..."
+    echo "Done."
+    sync
+}
+
+main
